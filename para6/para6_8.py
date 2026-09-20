947087058504149c188aa55fa8ad4269aba46452
user <sergusmisha@gmail.com>
2026-09-20 11:46:40 +0300

    introsperctions modules and classes

--- para6/para6_8.py
+++ para6/para6_8.py
@@ -4,11 +4,15 @@ class BuildingError(Exception):
 
 
 def check_material(amount_of_material, limit_value):
+    if "13" in str(amount_of_material):
+        print(f"This number: {amount_of_material} in ban!")
+        raise BuildingError(amount_of_material)
     if amount_of_material > limit_value:
         return "emough material"
     else:
         raise BuildingError(amount_of_material)
 
 
-material = 1200
+
+material = 1300
 check_material(material, 300)
